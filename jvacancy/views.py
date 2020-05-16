0fb519fa35a13116627d5b4a8cf8392e0a3e340f
Petrov Ruslan <petrov.r.e@gmail.com>
2020-05-16 12:58:27 +0200

    базовые views и routes

--- jvacancy/views.py
+++ jvacancy/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import Http404
+from django.views import View
 
 # Create your views here.
+
+class MainView(View):
+    def get(self, request, *args, **kwargs):
+        return HttpResponse("Это главная страница")
+
+class VacanciesView(View):
+    def get(self, request, *args, **kwargs):
+        return HttpResponse("Это список вакансий")
+
+class VacanciesCategoryView(View):
+    def get(self, request, *args, **kwargs):
+        return HttpResponse("Это вакансии по категориям")
+
+class VacancyView(View):
+    def get(self, request, *args, **kwargs):
+        return HttpResponse("Это страница вакансии")
+
