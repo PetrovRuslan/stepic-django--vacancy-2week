061956719f4403b0a3b49b0138f26fc0c55f059e
Petrov Ruslan <petrov.r.e@gmail.com>
2020-05-16 16:43:30 +0200

    набросок моделей

--- jvacancy/models.py
+++ jvacancy/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class Vacancy(models.Model):
+	id = models.AutoField(primary_key=True)
+	title = models.CharField(max_length=64)
+	specialty = models.ForeignKey(Specialty, on_delete=models.CASCADE, related_name='vacancies')
+	company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='vacancies')
+	skills = models.CharField(max_length=64)
+	description = models.CharField(max_length=256)
+	salary_min = models.IntegerField()
+	salary_max = models.IntegerField()
+	published_at = models.DateField()
+
+class Company(models.Model):
+	id = models.AutoField(primary_key=True)
+	name = models.CharField(max_length=64)
+	location = models.CharField(max_length=64)
+	logo = models.CharField(max_length=64)
+	description = models.CharField(max_length=256)
+	employee_count = models.IntegerField()
+
+class Specialty(models.Model):
+	id = models.AutoField(primary_key=True)
+	code = models.CharField(max_length=64)
+	title = models.CharField(max_length=64)
+	picture = models.CharField(max_length=128)
