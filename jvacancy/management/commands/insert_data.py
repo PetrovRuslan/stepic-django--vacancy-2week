742959ef63cbd9ab3bcfab6dd31443b6f2d322f5
Petrov Ruslan <petrov.r.e@gmail.com>
2020-05-17 22:40:45 +0200

    костыльная вставка данных в БД

--- jvacancy/management/commands/insert_data.py
+++ jvacancy/management/commands/insert_data.py
@@ -11,7 +11,18 @@ class Command(BaseCommand):
 	def handle(self, *args, **kwargs):
 		for company in companies:
 			company = Company.objects.create(name=company['title'], employee_count=randint(10, 200), logo="https://place-hold.it/100x60")
+
 		for specialty in specialties:
 			specialty = Specialty.objects.create(code=specialty['code'], title=specialty['title'], picture="https://place-hold.it/100x60")
-		for job in jobs:
-			job = Vacancy.objects.create(title=job['title'], specialty=specialty, company=company, salary_min=job['salary_from'], salary_max=job['salary_to'], published_at=job['posted'], description=job['desc'])
+
+		for company1 in companies:
+			for job in jobs:
+				if company1['title'] == job['company']:
+					company = Company.objects.get(name=company1['title'])
+					job = Vacancy.objects.create(title=job['title'], specialty=specialty, company=company, salary_min=job['salary_from'], salary_max=job['salary_to'], published_at=job['posted'], description=job['desc'])
+		
+		for specialty1 in specialties:
+			for job in jobs:
+				if specialty1['code'] == job['cat']:
+					specialty = Specialty.objects.get(code=specialty1['code'])
+					job = Vacancy.objects.update(specialty=specialty)
