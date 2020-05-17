daec751f345afe8771fa3b47095cb474deb2df16
Petrov Ruslan <petrov.r.e@gmail.com>
2020-05-17 14:09:47 +0200

    команда вставки данных в БД

--- jvacancy/models.py
+++ jvacancy/models.py
@@ -3,10 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Vacancy(models.Model):
-	id = models.AutoField(primary_key=True)
 	title = models.CharField(max_length=64)
-	specialty = models.ForeignKey(Specialty, on_delete=models.CASCADE, related_name='vacancies')
-	company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='vacancies')
+	specialty = models.ForeignKey('Specialty', on_delete=models.CASCADE, related_name='vacancies')
+	company = models.ForeignKey('Company', on_delete=models.CASCADE, related_name='vacancies')
 	skills = models.CharField(max_length=64)
 	description = models.CharField(max_length=256)
 	salary_min = models.IntegerField()
@@ -14,7 +13,6 @@ class Vacancy(models.Model):
 	published_at = models.DateField()
 
 class Company(models.Model):
-	id = models.AutoField(primary_key=True)
 	name = models.CharField(max_length=64)
 	location = models.CharField(max_length=64)
 	logo = models.CharField(max_length=64)
@@ -22,7 +20,6 @@ class Company(models.Model):
 	employee_count = models.IntegerField()
 
 class Specialty(models.Model):
-	id = models.AutoField(primary_key=True)
 	code = models.CharField(max_length=64)
 	title = models.CharField(max_length=64)
 	picture = models.CharField(max_length=128)
