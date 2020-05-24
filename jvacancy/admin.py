d662647d812228831732360fdc42aacd5f904223
Petrov Ruslan <petrov.r.e@gmail.com>
2020-05-25 00:57:29 +0200

    регистрация моделей в админке

--- jvacancy/admin.py
+++ jvacancy/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from jvacancy.models import Vacancy, Company, Application
 # Register your models here.
+
+class VacancyAdmin(admin.ModelAdmin):
+	pass
+
+class CompanyAdmin(admin.ModelAdmin):
+	pass
+
+class ApplicationAdmin(admin.ModelAdmin):
+	pass
+
+
+admin.site.register(Vacancy, VacancyAdmin)
+admin.site.register(Company, CompanyAdmin)
+admin.site.register(Application, ApplicationAdmin)
