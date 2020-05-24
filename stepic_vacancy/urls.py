501a656e66bc0c5ce64ea21d7081eb0021b7af5f
Petrov Ruslan <petrov.r.e@gmail.com>
2020-05-24 16:36:02 +0200

    добавлена раздача медиафайлов для тестовой среды

--- stepic_vacancy/urls.py
+++ stepic_vacancy/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from jvacancy.views import MainView, VacanciesView, VacanciesCategoryView, CompanyView, VacancyView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,3 +27,7 @@ urlpatterns = [
     path('company/<int:company>', CompanyView.as_view()),
     path('vacancies/<int:id>', VacancyView.as_view()),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, 
+                          document_root=settings.MEDIA_ROOT)
